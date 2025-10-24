77488bcae35a548c6bbaa49d6205c24ffcc77262
Pacific <pacific24k@gmail.com>
2025-10-24 10:52:10 +0530

    Post JSON addition

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
+import json
 app=FastAPI()
+
+def load_data():
+    with open("patients.json",'r') as f:
+        data=json.load(f)
+    return data
 
 @app.get("/")
 def hello():
-    return {"message":"Hello World"}
+    return {"message":"Patient Management System API"}
 
 @app.get("/about")
 def about():
-    return {"message":"We are in About Section!"}
+    return {"message":"A fully functional API to manage your patient records!"}
+
+@app.get("/view")
+def view():
+    data=load_data()
+    return data
+
